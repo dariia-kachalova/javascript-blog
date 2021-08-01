19492cbd2a0ebd7974c3cbf9d494f5821f29c612
Dariia Kachalova <danniednepr@gmail.com>
2021-08-01 17:21:35 +0200

    git add js/script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 'use strict';
 
-  function titleClickHandler (event){
+function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.titles a.active');
-
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  activeLink.clickedElement.add('active');
+  console.log('clickedElement:', clickedElement);
+  /* [DONE] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
